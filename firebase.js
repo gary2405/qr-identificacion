7e7f5c225a1480d623d677f15de2a7f5d7aa9214
gary2405 <168210285+gary2405@users.noreply.github.com>
2026-02-05 00:10:58 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbMcYZ0BcsUSTTXvxbe5WNaKfVUsbM_Rk",
+  authDomain: "qr-identificacion.firebaseapp.com",
+  databaseURL: "https://qr-identificacion-default-rtdb.firebaseio.com",
+  projectId: "qr-identificacion",
+  storageBucket: "qr-identificacion.firebasestorage.app",
+  messagingSenderId: "331843010145",
+  appId: "1:331843010145:web:8b064c80fe57c06d53f420"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
