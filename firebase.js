1eb0ca0819692e2bd3743bf3e8bd61147bc42ba7
Gary Alvarado <garychaves25@gmail.com>
2026-02-06 21:52:56 -0600

    actualizacion con la foto de usuario

--- firebase.js
+++ firebase.js
@@ -1,22 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbMcYZ0BcsUSTTXvxbe5WNaKfVUsbM_Rk",
+  apiKey: "TU_API_KEY",
   authDomain: "qr-identificacion.firebaseapp.com",
   databaseURL: "https://qr-identificacion-default-rtdb.firebaseio.com",
   projectId: "qr-identificacion",
-  storageBucket: "qr-identificacion.firebasestorage.app",
+  storageBucket: "qr-identificacion.appspot.com",
   messagingSenderId: "331843010145",
   appId: "1:331843010145:web:8b064c80fe57c06d53f420"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
 
-export { db };
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+export { db, storage };
