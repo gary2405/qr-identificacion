5481da8473bc7e04be2133fa900ac1d474b5b4c8
Gary Alvarado <garychaves25@gmail.com>
2026-03-26 12:30:02 -0600

    nuevos cambis y funciones

--- ver.js
+++ ver.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase.js";
-import { ref, get, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { ref, get } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const qrId = new URLSearchParams(window.location.search).get("id");
 
@@ -18,15 +18,11 @@ get(ref(db,"qrs/"+qrId)).then(snap=>{
 
   nombre.textContent = d.nombre;
   tipo.textContent = d.tipoPerfil;
-
   direccion.textContent = d.ubicacion || d.direccion || "";
-
   mensaje.textContent = d.mensaje;
 
   foto.src = d.foto || "https://cdn-icons-png.flaticon.com/512/149/149071.png";
-  foto.classList.remove("qr-oculto");
 
-  /* PERSONA */
   if(d.sangre || d.padecimientos || d.alergias){
     verPersona.classList.remove("qr-oculto");
     document.getElementById("verSangre").textContent = d.sangre || "";
@@ -34,20 +30,6 @@ get(ref(db,"qrs/"+qrId)).then(snap=>{
     document.getElementById("verAlergias").textContent = d.alergias || "";
   }
 
-  if(d.personaExtra){
-    const p = d.personaExtra;
-    const div = document.createElement("div");
-    div.innerHTML = `
-      ${p.edad ? `<p>Edad: ${p.edad}</p>`:""}
-      ${p.estatura ? `<p>Estatura: ${p.estatura}</p>`:""}
-      ${p.peso ? `<p>Peso: ${p.peso}</p>`:""}
-      ${p.cabello ? `<p>Cabello: ${p.cabello}</p>`:""}
-      ${p.ojos ? `<p>Ojos: ${p.ojos}</p>`:""}
-    `;
-    document.querySelector(".perfil-card").appendChild(div);
-  }
-
-  /* MASCOTA */
   if(d.mascota){
     verMascota.classList.remove("qr-oculto");
     document.getElementById("verEspecie").textContent = d.mascota.especie;
@@ -55,23 +37,9 @@ get(ref(db,"qrs/"+qrId)).then(snap=>{
     document.getElementById("verColor").textContent = d.mascota.color;
   }
 
-  if(d.mascotaExtra){
-    if(d.mascotaExtra.estado==="perdido"){
-      document.body.style.background="#ffe5e5";
-    }
-
-    if(d.mascotaExtra.recompensa){
-      const r=document.createElement("p");
-      r.innerHTML=`<b>Recompensa:</b> ${d.mascotaExtra.recompensa}`;
-      document.querySelector(".perfil-card").appendChild(r);
-    }
-  }
-
-  /* OBJETO */
   if(d.objeto){
     verObjeto.classList.remove("qr-oculto");
     document.getElementById("verDescripcion").textContent = d.objeto.descripcion;
     document.getElementById("verInstrucciones").textContent = d.objeto.instrucciones;
   }
-
 });
